3e73b195b4973e949836e58ea04851f8df899588
Germain d'Udekem <germain.dudekem@gmail.com>
2023-01-31 16:35:48 -0600

    added hilt and SignUpViewModel

---
+++ app/src/main/java/com/example/smallworld/MainActivity.kt
@@ -0,0 +1,71 @@
+package com.example.smallworld
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.smallworld.ui.SignUpScreen
+import com.example.smallworld.ui.SignUpViewModel
+import com.example.smallworld.ui.theme.SmallWorldTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+
+    private val signUpViewModel: SignUpViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SmallWorldTheme {
+                // A surface container using the 'background' color from the theme
+                SmallWorldApp()
+            }
+        }
+    }
+
+}
+
+@Composable
+fun SmallWorldApp() {
+    val navController = rememberNavController()
+
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        SmallWorldNavHost(navHostController = navController)
+    }
+}
+
+@Composable
+fun SmallWorldNavHost(
+    navHostController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController = navHostController, startDestination = "signup", modifier = modifier
+    ) {
+        composable("signup") {
+            SignUpScreen()
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    SmallWorldTheme {
+        SignUpScreen()
+    }
+}
---
+++ app/src/main/java/com/example/smallworld/SmallWorldApplication.kt
@@ -0,0 +1,7 @@
+package com.example.smallworld
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class SmallWorldApplication : Application()
