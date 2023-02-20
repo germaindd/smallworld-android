6de61c34030186de7ccabbd43271f2fdfb34d370
Germain d'Udekem <germain.dudekem@gmail.com>
2023-02-20 01:16:54 -0600

    get api base url from strings for debug builds to aid development

--- app/src/main/java/com/example/smallworld/di/AppModule.kt
+++ app/src/main/java/com/example/smallworld/di/AppModule.kt
@@ -1,12 +1,15 @@
 package com.example.smallworld.di
 
+import android.content.Context
 import com.example.smallworld.BuildConfig
+import com.example.smallworld.R
 import com.example.smallworld.data.SmallWorldApi
 import com.example.smallworld.data.SmallWorldAuthApi
 import com.example.smallworld.data.auth.AuthOkhttpInterceptor
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import kotlinx.coroutines.*
 import okhttp3.OkHttpClient
@@ -23,6 +26,10 @@ annotation class DispatcherIO
 @Retention(AnnotationRetention.BINARY)
 annotation class ApplicationScope
 
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class BaseUrl
+
 @InstallIn(SingletonComponent::class)
 @Module
 class AppModule {
@@ -32,9 +39,17 @@ class AppModule {
 
     @Singleton
     @Provides
-    fun provideAuthApi(okhttp: OkHttpClient): SmallWorldAuthApi =
+    @BaseUrl
+    fun provideApiBaseUrl(@ApplicationContext context: Context): String = when (BuildConfig.DEBUG) {
+        true -> context.getString(R.string.api_url)
+        false -> BuildConfig.API_BASE_URL
+    }
+
+    @Singleton
+    @Provides
+    fun provideAuthApi(okhttp: OkHttpClient, @BaseUrl baseUrl: String): SmallWorldAuthApi =
         Retrofit.Builder()
-            .baseUrl("${BuildConfig.API_BASE_URL}auth/")
+            .baseUrl("$baseUrl/auth/")
             .client(okhttp)
             .addConverterFactory(MoshiConverterFactory.create())
             .build()
@@ -42,9 +57,12 @@ class AppModule {
 
     @Singleton
     @Provides
-    fun provideApi(authInterceptor: AuthOkhttpInterceptor): SmallWorldApi =
+    fun provideApi(
+        authInterceptor: AuthOkhttpInterceptor,
+        @BaseUrl baseUrl: String
+    ): SmallWorldApi =
         Retrofit.Builder()
-            .baseUrl(BuildConfig.API_BASE_URL)
+            .baseUrl(baseUrl)
             .client(OkHttpClient.Builder().addInterceptor(authInterceptor).build())
             .addConverterFactory(MoshiConverterFactory.create())
             .build()
